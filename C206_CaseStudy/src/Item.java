3fb834a75a89a6c9d59d4b63ae7fc0be5f56ce6b
Axton Ather <Axton Ather@DESKTOP-00521VT>
2022-08-02 09:58:01 +0800

    Commit and Push into GitHub

--- C206_CaseStudy/src/Item.java
+++ C206_CaseStudy/src/Item.java
@@ -1,4 +1,71 @@
 
 public class Item {
 
+	private String name;
+	private String description;
+	private double mininum_bid_price;
+	private boolean isAvailable;
+	private String auctionstartdate;
+	private String end_date;
+
+	public Item(String name, String description) {
+		this.name = name;
+		this.description = description;
+		this.mininum_bid_price = mininum_bid_price;
+		this.isAvailable = isAvailable;
+		this.auctionstartdate = auctionstartdate;
+		this.end_date = end_date;
+	}
+
+	public String toString() {
+		String itemInfo = String.format("%-10s %-30s %-10s %-10s %-10s %-10s,", name, description,
+				showAvailability(isAvailable), auctionstartdate, end_date);
+
+		return itemInfo;
+	}
+
+	private Object showAvailability(boolean isAvailable2) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public String getAuctionstartdate() {
+		return auctionstartdate;
+	}
+
+	public void setAuctionstartdate(String auctionstartdate) {
+		this.auctionstartdate = auctionstartdate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getMininum_bid_price() {
+		return mininum_bid_price;
+	}
+
+	public String getEnd_date() {
+		return end_date;
+	}
+
 }
